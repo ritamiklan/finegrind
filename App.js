80774503945d43cc543e61d1b43b16399b50d8d5
ritamiklan <51336470+ritamiklan@users.noreply.github.com>
2022-02-08 14:46:33 +0200

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, FlatList, Text, View } from 'react-native';
 
 export default function App() {
+  const [coffeeList, setCoffeeList] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.finegrind.app/cafes/')
+    .then((response) => response.json())
+    .then(dt => setCoffeeList(dt.data))
+    .catch(err => Alert.alert('Something went wrong'));
+  }
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Text>Welcome!</Text>
+      </View>
+      <View style={{flex: 2}}>
+      <FlatList
+          data={coffeeList}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.attributes.name}</Text>
+          )}
+      />
+       
+      </View>
     </View>
   );
 }
