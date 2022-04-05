a7c78278195df5307657c1a1963fcda732e509f6
ritamiklan <51336470+ritamiklan@users.noreply.github.com>
2022-04-05 08:44:55 +0300

    added dotenv

--- App.js
+++ App.js
@@ -1,39 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, FlatList, Text, View, Image } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, FlatList, Text, View, Image } from "react-native";
+import config from "./config";
 
 export default function App() {
   const [coffeeList, setCoffeeList] = useState([]);
 
-
   // should only render on the first load
   useEffect(() => {
-    fetch('https://api.finegrind.app/cafes/')
-    .then((response) => response.json())
-    .then(dt => setCoffeeList(dt.data))
-    .catch(err => Alert.alert('Something went wrong'));
-  }
-  );
+    fetch(config.API_URL)
+      .then((response) => response.json())
+      .then((dt) => setCoffeeList(dt.data))
+      .catch((err) => Alert.alert("Something went wrong"));
+  });
 
   return (
     <View style={styles.container}>
-      <View style={{flex: 1, justifyContent: 'center'}}>
+      <View style={{ flex: 1, justifyContent: "center" }}>
         <Text>Welcome!</Text>
       </View>
-      <View style={{flex: 2}}>
-      <FlatList
+      <View style={{ flex: 2 }}>
+        <FlatList
           data={coffeeList}
           keyExtractor={({ id }, index) => id}
           renderItem={({ item }) => (
             <View>
               <Text>{item.attributes.name}</Text>
               <Image
-                  style={{ width: 50, height: 50 }} 
-                  source={{ uri: `${item.attributes.image}` }} 
-                />
+                style={{ width: 50, height: 50 }}
+                source={{ uri: `${item.attributes.image}` }}
+              />
             </View>
           )}
-      />
-       
+        />
       </View>
     </View>
   );
@@ -42,9 +40,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
@@ -55,4 +53,3 @@ const styles = StyleSheet.create({
 - make UI somewhat nice-ish
 
 */
-
