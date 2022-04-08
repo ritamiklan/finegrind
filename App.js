e1ab8afecfe78f2da492ac100d4a597965432f28
ritamiklan <51336470+ritamiklan@users.noreply.github.com>
2022-04-08 13:34:16 +0300

    starting on navigation

--- App.js
+++ App.js
@@ -1,50 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, FlatList, Text, View, Image } from "react-native";
 import config from "./src/utils/config";
+import CoffeeList from "./src/components/CoffeeList";
 
 export default function App() {
-  const [coffeeList, setCoffeeList] = useState([]);
-
-  // should only render on the first load
-  useEffect(() => {
-    fetch(config.API_URL)
-      .then((response) => response.json())
-      .then((dt) => setCoffeeList(dt.data))
-      .catch((err) => Alert.alert("Something went wrong"));
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <View style={{ flex: 1, justifyContent: "center" }}>
-        <Text>Welcome!</Text>
-      </View>
-      <View style={{ flex: 2 }}>
-        <FlatList
-          data={coffeeList}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <View>
-              <Text>{item.attributes.name}</Text>
-              <Image
-                style={{ width: 50, height: 50 }}
-                source={{ uri: `${item.attributes.image}` }}
-              />
-            </View>
-          )}
-        />
-      </View>
-    </View>
-  );
+  return <CoffeeList />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 /* 
 
--- src/components/CoffeeList.js
+++ src/components/CoffeeList.js
@@ -3,5 +3,45 @@ import { StyleSheet, FlatList, Text, View, Image } from "react-native";
 import config from "../utils/config";
 
 export default function CoffeeList() {
-  return <View></View>;
+  const [coffeeList, setCoffeeList] = useState([]);
+
+  // should only render on the first load
+  useEffect(() => {
+    fetch(config.API_URL)
+      .then((response) => response.json())
+      .then((dt) => setCoffeeList(dt.data))
+      .catch((err) => Alert.alert("Something went wrong"));
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <Text>Welcome!</Text>
+      </View>
+      <View style={{ flex: 2 }}>
+        <FlatList
+          data={coffeeList}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <View>
+              <Text>{item.attributes.name}</Text>
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={{ uri: `${item.attributes.image}` }}
+              />
+            </View>
+          )}
+        />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
