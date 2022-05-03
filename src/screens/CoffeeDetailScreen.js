7e3b696a0e116c2249e480c417ab111dd0c7a912
ritamiklan <51336470+ritamiklan@users.noreply.github.com>
2022-05-03 17:47:43 +0300

    coffee detail screen starts to shape up

--- src/screens/CoffeeDetailScreen.js
+++ src/screens/CoffeeDetailScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, Button } from "react-native";
 import config from "../utils/config";
 
 // detailed page for every individual cafe with all the details, such as name, opening hrs and so on
@@ -11,8 +11,19 @@ export default function CoffeeDetailScreen({ route }) {
     id: "",
     attributes: {
       name: "",
+      description: "",
       address: "",
+      roastery: "",
       image: "../../assets/coffee.jpg",
+      openinghours: {
+        mon: "",
+        tue: "",
+        wed: "",
+        thu: "",
+        fri: "",
+        sat: "",
+        sun: "",
+      },
     },
   });
 
@@ -30,15 +41,51 @@ export default function CoffeeDetailScreen({ route }) {
   }, []);
 
   return (
-    <View>
-      <Text>{cafe.attributes.name}</Text>
-      <Text>{cafe.attributes.description}</Text>
+    <View style={styles.container}>
       <Image
-        style={{ width: 150, height: 100 }}
+        style={styles.image}
         source={{ uri: `${cafe.attributes.image}` }}
       />
+      <View style={styles.details}>
+        <Text>{cafe.attributes.name}</Text>
+        <Text>{cafe.attributes.description}</Text>
+        <Text>Hours: {cafe.attributes.openinghours.mon}</Text>
+        <Text>Roastery: {cafe.attributes.roastery}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          style={styles.button}
+          title="View on map"
+          onPress={() => console.log("pushed")}
+        />
+        <Button
+          style={styles.button}
+          title="IDK"
+          onPress={() => console.log("idk")}
+        />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: "auto",
+    height: 150,
+  },
+  details: {
+    flex: 2,
+    marginHorizontal: 10,
+    justifyContent: "space-around",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 50,
+  },
+  button: {},
+});
