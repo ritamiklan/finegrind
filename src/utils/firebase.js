01d49f277b3ed4a637e59463460fdda91f997602
ritamiklan <51336470+ritamiklan@users.noreply.github.com>
2022-05-06 11:37:22 +0300

    firebase setup

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import config from "./config";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_DENDER_ID,
+  appId: FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
