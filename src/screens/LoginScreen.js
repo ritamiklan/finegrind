ed83c1e58b3e2b4df706543c5ec3df85aa802ef0
ritamiklan <51336470+ritamiklan@users.noreply.github.com>
2022-05-06 15:23:13 +0300

    firebase authentication progress

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import { View, TextInput, Button, KeyboardAvoidingView } from "react-native";
 import { globalStyles } from "../styles/global";
+import firebaseApp from "../utils/firebase";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 // this is just a boilerplate code so it's easy to copy/paste when a new screen or component is added
 
@@ -9,30 +15,54 @@ export default function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const auth = getAuth(firebaseApp);
+
+  const handleSignup = () => {
+    createUserWithEmailAndPassword(auth, email, password).then(
+      (userCredentioals) => {
+        const user = userCredentioals.user;
+        console.log("registered with", user.email);
+      }
+    );
+  };
+
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, password).then(
+      (userCredentioals) => {
+        const user = userCredentioals.user;
+        console.log("logged in with", user.email);
+      }
+    );
+  };
+
   return (
-    <KeyboardAvoidingView style={globalStyles.loginContainer}>
-      <TextInput
-        style={globalStyles.inputContainer}
-        placeholder="Username"
-        value={username}
-        onChangeText={(text) => setUsername(text)}
-      />
-      <TextInput
-        style={globalStyles.inputContainer}
-        placeholder="E-mail"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput
-        style={globalStyles.inputContainer}
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
+    <KeyboardAvoidingView style={globalStyles.container}>
+      <View style={globalStyles.inputContainer}>
+        <TextInput
+          style={globalStyles.inputField}
+          placeholder="Username"
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+        />
+        <TextInput
+          style={globalStyles.inputField}
+          placeholder="E-mail"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={globalStyles.inputField}
+          placeholder="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          autoCapitalize="none"
+        />
+      </View>
       <View style={globalStyles.buttonContainer}>
-        <Button title="Login" onPress={() => console.log("login")} />
-        <Button title="Sign up" onPress={() => console.log("sign up")} />
+        <Button title="Login" onPress={handleLogin} />
+        <Button title="Sign up" onPress={handleSignup} />
       </View>
     </KeyboardAvoidingView>
   );
--- src/styles/global.js
+++ src/styles/global.js
@@ -3,6 +3,7 @@ import { StyleSheet } from "react-native";
 export const globalStyles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
   },
   loginContainer: {
     flex: 1,
@@ -10,8 +11,18 @@ export const globalStyles = StyleSheet.create({
     alignItems: "center",
   },
   inputContainer: {
+    width: "80%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: 40,
+  },
+  inputField: {
     borderWidth: 1,
     borderColor: "#333",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginTop: 10,
+    width: "100%",
   },
   image: {
     flex: 1,
