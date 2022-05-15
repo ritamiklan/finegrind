a3b27e233b1267c3cfe4d10eef1abd1eda75fb08
ritamiklan <51336470+ritamiklan@users.noreply.github.com>
2022-05-15 09:26:46 +0300

    fav button conditional rendering

--- src/screens/CoffeeDetailScreen.js
+++ src/screens/CoffeeDetailScreen.js
@@ -2,13 +2,48 @@ import React from "react";
 import { View, Text, Image, Button, TouchableOpacity } from "react-native";
 import useId from "../hooks/useId";
 import { globalStyles } from "../styles/global";
+import { useUser } from "../context/UserContext";
 
 // detailed page for every individual cafe with all the details, such as name, opening hrs and so on
 
 export default function CoffeeDetailScreen({ route, navigation }) {
   const { id } = route.params;
 
+  const { isLoggedIn } = useUser();
+
   const [coffeeDetail] = useId(id);
+
+  let buttons;
+  if (isLoggedIn) {
+    buttons = (
+      <View style={globalStyles.buttonContainer}>
+        <Button
+          style={globalStyles.button}
+          title="View on map"
+          onPress={() =>
+            navigation.navigate("ShowMap", { id: id, data: coffeeDetail })
+          }
+        />
+        <Button
+          style={globalStyles.button}
+          title="Add to favs"
+          onPress={() => console.log("add to favs", id)}
+        />
+      </View>
+    );
+  } else {
+    buttons = (
+      <View style={globalStyles.buttonContainer}>
+        <Button
+          style={globalStyles.button}
+          title="View on map"
+          onPress={() =>
+            navigation.navigate("ShowMap", { id: id, data: coffeeDetail })
+          }
+        />
+      </View>
+    );
+  }
 
   return (
     <View style={globalStyles.container}>
@@ -33,20 +68,7 @@ export default function CoffeeDetailScreen({ route, navigation }) {
         </TouchableOpacity> */}
         <Text>Roastery: {coffeeDetail.attributes.roasteries[0].name}</Text>
       </View>
-      <View style={globalStyles.buttonContainer}>
-        <Button
-          style={globalStyles.button}
-          title="View on map"
-          onPress={() =>
-            navigation.navigate("ShowMap", { id: id, data: coffeeDetail })
-          }
-        />
-        <Button
-          style={globalStyles.button}
-          title="Add to favs"
-          onPress={() => console.log("add to favs", id)}
-        />
-      </View>
+      {buttons}
     </View>
   );
 }
