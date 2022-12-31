480a7a2923b21f1fc858723d2d5dfd3ec9c8a0f6
Rita Miklán <rita.miklan@Ritas-MacBook-Pro-2.local>
2022-12-31 15:40:49 +0200

    update stuff

--- App.js
+++ App.js
@@ -1,123 +1,16 @@
-import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "./src/screens/HomeScreen";
-import CoffeeListScreen from "./src/screens/CoffeeListScreen";
-import CoffeeDetailScreen from "./src/screens/CoffeeDetailScreen";
-import ShowMap from "./src/screens/ShowMapScreen";
-import RegisterUserScreen from "./src/screens/RegisterUserScreen";
-import LoginScreen from "./src/screens/LoginScreen";
+
 import UserProvider from "./src/context/UserContext";
-import UserProfile from "./src/screens/UserProfileScreen";
 import { SafeAreaView } from "react-native-safe-area-context";
+import HomeNav from "./src/navigation/StackNavigation";
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
   return (
     <UserProvider>
       <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
         <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen
-              name="Home"
-              component={Home}
-              options={{
-                title: "Home",
-                headerStyle: {
-                  backgroundColor: "#5F7161",
-                },
-                headerTintColor: "#EFEAD8",
-                headerTitleStyle: {
-                  fontWeight: "bold",
-                },
-              }}
-            />
-            <Stack.Screen
-              name="CoffeeListScreen"
-              component={CoffeeListScreen}
-              options={{
-                title: "Cafés",
-                headerStyle: {
-                  backgroundColor: "#5F7161",
-                },
-                headerTintColor: "#EFEAD8",
-                headerTitleStyle: {
-                  fontWeight: "bold",
-                },
-              }}
-            />
-            <Stack.Screen
-              name="ShowMap"
-              component={ShowMap}
-              options={{
-                title: "Map",
-                headerStyle: {
-                  backgroundColor: "#5F7161",
-                },
-                headerTintColor: "#EFEAD8",
-                headerTitleStyle: {
-                  fontWeight: "bold",
-                },
-              }}
-            />
-            <Stack.Screen
-              name="CoffeeDetailScreen"
-              component={CoffeeDetailScreen}
-              options={{
-                title: "Info",
-                headerStyle: {
-                  backgroundColor: "#5F7161",
-                },
-                headerTintColor: "#EFEAD8",
-                headerTitleStyle: {
-                  fontWeight: "bold",
-                },
-              }}
-            />
-            <Stack.Screen
-              name="RegisterUserScreen"
-              component={RegisterUserScreen}
-              options={{
-                title: "Register",
-                headerStyle: {
-                  backgroundColor: "#5F7161",
-                },
-                headerTintColor: "#EFEAD8",
-                headerTitleStyle: {
-                  fontWeight: "bold",
-                },
-              }}
-            />
-            <Stack.Screen
-              name="LoginScreen"
-              component={LoginScreen}
-              options={{
-                title: "Log in",
-                headerStyle: {
-                  backgroundColor: "#5F7161",
-                },
-                headerTintColor: "#EFEAD8",
-                headerTitleStyle: {
-                  fontWeight: "bold",
-                },
-              }}
-            />
-            <Stack.Screen
-              name="UserProfile"
-              component={UserProfile}
-              options={{
-                title: "Profile",
-                headerStyle: {
-                  backgroundColor: "#5F7161",
-                },
-                headerTintColor: "#EFEAD8",
-                headerTitleStyle: {
-                  fontWeight: "bold",
-                },
-              }}
-            />
-          </Stack.Navigator>
+          <HomeNav />
         </NavigationContainer>
       </SafeAreaView>
     </UserProvider>
