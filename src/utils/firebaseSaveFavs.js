63cb3659dfb528782a3cab9a2c14f3b9db76da44
Rita Miklan <ritamiklan@gmail.com>
2023-03-19 17:00:11 +0200

    remove from favs wip

--- src/utils/firebaseSaveFavs.js
+++ src/utils/firebaseSaveFavs.js
@@ -1,10 +1,18 @@
 import { getDatabase, ref, update } from "firebase/database";
 import firebaseApp from "./firebase";
 
-export default function firebaseSaveFavs(user, favs, uid) {
+export function firebaseSaveFavs(user, favs, uid) {
   const db = getDatabase(firebaseApp);
 
   update(ref(db, "users/" + uid), {
+    favs: favs,
+  });
+}
+
+export function firebaseRemoveFavs(user, favs, uid) {
+  const db = getDatabase(firebaseApp);
+
+  remove(ref(db, "users/" + uid), {
     favs: favs,
   });
 }
